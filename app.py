0be96975b7fb11eb1d5592c5ebbdb694e8609f51
Min Thura Zaw <minthurazaw619@gmail.com>
2024-05-22 18:29:16 +0700

    update html files

--- app.py
+++ app.py
@@ -3,21 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Sample data
-# tables = {
-#     "Table1": [
-#         {"id": 1, "name": "Alice", "age": 30},
-#         {"id": 2, "name": "Bob", "age": 25}
-#     ],
-#     "Table2": [
-#         {"id": 1, "product": "Widget", "price": 19.99},
-#         {"id": 2, "product": "Gadget", "price": 22.50}
-#     ]
-# }
+tables = {
+    "Table1": [
+        {"id": 1, "name": "Alice", "age": 30},
+        {"id": 2, "name": "Bob", "age": 25}
+    ],
+    "Table2": [
+        {"id": 1, "product": "Widget", "price": 19.99},
+        {"id": 2, "product": "Gadget", "price": 22.50}
+    ]
+}
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', tables=tables.keys())
 
 
 if __name__ == '__main__':
