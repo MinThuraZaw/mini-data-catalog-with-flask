1b7bfe260a5713d9b28609e4e925dee27adb6345
Min Thura Zaw <minthurazaw619@gmail.com>
2024-05-23 20:33:26 +0700

    update add item process

--- static/script.js
+++ static/script.js
@@ -1,23 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var modal = document.getElementById('tableModal');
-    var span = document.getElementsByClassName('close')[0];
+    var tableModal = document.getElementById('tableModal');
+    var addTableModal = document.getElementById('addTableModal');
     var tableDetails = document.getElementById('tableDetails');
+    var tableSpan = document.getElementsByClassName('close')[0];
+    var addTableSpan = document.getElementsByClassName('close')[1];
+    var openAddTableModalBtn = document.getElementById('openAddTableModal');
+//    var closeButtons = document.querySelectorAll('.close');
+
 
     document.querySelectorAll('.table-item').forEach(function(item) {
         item.addEventListener('click', function() {
             var tableName = this.getAttribute('data-table-name');
             tableDetails.textContent = 'Details for table: ' + tableName;
-            modal.style.display = 'block';
+            tableModal.style.display = 'block';
         });
     });
 
-    span.onclick = function () {
-        modal.style.display = 'none';
+    // Close modals when the close button is clicked
+//    closeButtons.forEach(function(btn) {
+//        btn.onclick = function () {
+//            this.closest('.modal').style.display = 'none';
+//        };
+//    });
+
+    tableSpan.onclick = function () {
+        tableModal.style.display = 'none';
+    }
+
+    addTableSpan.onclick = function () {
+        addTableModal.style.display = 'none';
     }
 
     window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
+        if (event.target == tableModal) {
+            tableModal.style.display = 'none';
+        } else if (event.target == addTableModal) {
+            addTableModal.style.display = 'none';
         }
+    }
+
+    openAddTableModalBtn.onclick = function () {
+        addTableModal.style.display = 'block';
     }
 });
